3da410648e3bdfbe998520237491e3c861e57162
Timothy Webb <tim@genuitec.com>
2014-02-12 15:06:06 -0600

    Running SWT HelloWorld on Raspberry Pi!

--- com.genuitec.piplug.poc/src/com/genuitec/piplug/poc/PiPlugPOCActivator.java
+++ com.genuitec.piplug.poc/src/com/genuitec/piplug/poc/PiPlugPOCActivator.java
@@ -3,7 +3,7 @@ package com.genuitec.piplug.poc;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+public class PiPlugPOCActivator implements BundleActivator {
 
 	private static BundleContext context;
 
@@ -16,7 +16,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		PiPlugPOCActivator.context = bundleContext;
 	}
 
 	/*
@@ -24,7 +24,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		PiPlugPOCActivator.context = null;
 	}
 
 }
