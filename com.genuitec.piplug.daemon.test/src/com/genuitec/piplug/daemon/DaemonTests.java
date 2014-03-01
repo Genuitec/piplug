f27e59e539f519d2bb378e91f246f0854612a06b
Timothy Webb <tim@genuitec.com>
2014-02-28 19:06:10 -0600

    Fix workaround for Java VM bug

--- com.genuitec.piplug.daemon.test/src/com/genuitec/piplug/daemon/DaemonTests.java
+++ com.genuitec.piplug.daemon.test/src/com/genuitec/piplug/daemon/DaemonTests.java
@@ -49,6 +49,7 @@ public class DaemonTests {
 			}
 		    }
 		} catch (Exception ignore) {
+		    ignore.printStackTrace();
 		    // best effort
 		}
 	    }
@@ -56,26 +57,62 @@ public class DaemonTests {
 
 	private void sendDiscovery(InterfaceAddress ip) {
 	    InetAddress broadcast = ip.getBroadcast();
-	    sendDiscoveryTo(broadcast);
 
 	    // see if we are getting hit by:
 	    // http://bugs.java.com/bugdatabase/view_bug.do?bug_id=7158636
-	    if (ip.getAddress().getAddress()[0] != ip.getBroadcast()
-		    .getAddress()[0]) {
+	    if (ip.getAddress().getAddress()[0] == broadcast.getAddress()[0]) {
 
-		// change the last segment to .255
-		byte[] addr = ip.getAddress().getAddress();
-		addr[3] = (byte) 255;
-		try {
-		    sendDiscoveryTo(InetAddress.getByAddress(addr));
-		} catch (UnknownHostException ignored) {
+		// broadcast passes the sniff test for the bug, just use what is
+		// provided by the JVM
+		sendDiscoveryTo(broadcast);
+
+	    } else {
+
+		// hacks to see if we can discover the correct broadcast address
+		// thanks to windows WLAN adapters under Java not always
+		// returning the right address for us
+
+		// see if we can fix the address by network prefix
+		if (ip.getNetworkPrefixLength() >= 8) {
+
+		    byte[] addr = ip.getAddress().getAddress();
+		    StringBuffer bitMask = new StringBuffer();
+		    int i = 0;
+		    while (i < ip.getNetworkPrefixLength()) {
+			bitMask.append('0');
+			i++;
+		    }
+		    while (i < 32) {
+			bitMask.append('1');
+			i++;
+		    }
+		    for (i = 0; i < 4; i++) {
+			String maskStr = bitMask.substring(i * 8, (i + 1) * 8);
+			byte mask = (byte) (Integer.parseInt(maskStr, 2) & 0xff);
+			addr[i] = (byte) ((addr[i] | mask) & 0xff);
+		    }
+		    try {
+			sendDiscoveryTo(InetAddress.getByAddress(addr));
+		    } catch (UnknownHostException ignored) {
+		    }
+
 		}
 
-		// change the last two segments to .255.255
-		addr[2] = (byte) 255;
-		try {
-		    sendDiscoveryTo(InetAddress.getByAddress(addr));
-		} catch (UnknownHostException ignored) {
+		if (1 == 2) {
+		    // change the last segment to .255
+		    byte[] addr = ip.getAddress().getAddress();
+		    addr[3] = (byte) 255;
+		    try {
+			sendDiscoveryTo(InetAddress.getByAddress(addr));
+		    } catch (UnknownHostException ignored) {
+		    }
+
+		    // change the last two segments to .255.255
+		    addr[2] = (byte) 255;
+		    try {
+			sendDiscoveryTo(InetAddress.getByAddress(addr));
+		    } catch (UnknownHostException ignored) {
+		    }
 		}
 	    }
 	}
